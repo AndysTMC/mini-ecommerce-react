12a2cbdeae970898637694d0e670b2221a6f6995
AndysTMC <thatikondamukesh08@gmail.com>
2024-09-19 20:23:04 +0530

    Fixes

--- src/api_services/urls.js
+++ src/api_services/urls.js
@@ -1,3 +1,3 @@
-// let backendUrl = "https://mini-ecommerce-km8f.onrender.com";
-let backendUrl = "http://localhost:8080";
+let backendUrl = "https://mini-ecommerce-km8f.onrender.com";
+// let backendUrl = "http://localhost:8080";
 export { backendUrl }
